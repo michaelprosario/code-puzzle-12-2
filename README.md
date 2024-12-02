711b2950c9bdc924c6a6028595a26799f77e403a
michaelprosario <michael.p.rosario@gmail.com>
2024-12-02 09:26:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# code-puzzle-12-2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/michaelprosario/code-puzzle-12-2)
---
+++ index.js
@@ -0,0 +1,11 @@
+function firstNonRepeatingChar(str) {
+  return '';
+}
+
+// test case 1
+const answer = firstNonRepeatingChar('aaaaabbbbbczzzzzztkkkkkkk1');
+if (answer === 'c') {
+  console.log('pass');
+} else {
+  console.log('fail');
+}
